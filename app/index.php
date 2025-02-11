f368ee4aee020c1ebb3591fd98e3baa2470335e9
Daniel Heezen <146465@student.dcterra.nl>
2025-02-11 10:47:01 +0100

    form
    form gemaakt die nog niets doet maar heeft wel styling

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "mysql"; // Le host est le nom du service, présent dans le docker-compose.yml
+$host = "mysql";
 $dbname = "my-wonderful-website";
 $charset = "utf8";
 $port = "3306";
@@ -9,8 +9,27 @@ $port = "3306";
 <html>
 <head>
     <title>Drenthe College docker web server</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
+
 <body>
-<h2>Hello world!</h2>
+
+    <?php
+
+    spl_autoload_register(function($class_name) {
+        include "classes/" . $class_name . ".php";
+    });
+
+    ?>
+
+    <div id="form-Container">
+
+        <form method="POST">
+            <div><input class="username" placeholder="Username" required></div>
+            <div><input class="password" type="password" placeholder="Password" required></div>
+            <div><input class="submit" type="submit" name="submit" value="Sumbit" ></div>
+        </form>
+
+    </div>
 </body>
 </html>
